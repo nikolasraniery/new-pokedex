0f5882259bb4c2f1adff0bb060b58a7101f0ecff
nikolasraniery <nikolassraniery@gmail.com>
2024-04-24 12:53:27 -0300

    [PD-01] - feat: Criando e estilizando novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="w-full h-screen flex flex-col items-center ">
+    <div className="w-full h-screen flex flex-col items-center bg-cream">
       <div className="w-full">
         <Navbar />
       </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,23 @@
 export function Navbar() {
   return (
-    <div className="w-full flex justify-center items-center bg-red-primary h-16">
-      <ul className="flex gap-6 w-full justify-center text-cream">
-        <li className=":hover::after-w-full ::after-content-['']">Pokemons</li>
-        <li className="cursor-pointer">Itens</li>
-        <li className="cursor-pointer">Localizações</li>
+    <div className="w-full flex justify-center items-center bg-red-primary h-16 p-8">
+      <h1 className="font-pokemon text-3xl text-yellow-main bg-blue-main p-2 pb-0 rounded-md border-2 border-black">
+        Pokédex
+      </h1>
+      <ul className="flex gap-12 w-full justify-center text-cream font-pressStart">
+        <li className="cursor-pointer relative group hover:after:w-full text-12 font-normal">
+          Pokémons
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-yellow-main opacity-0 transition-all duration-400 group-hover:opacity-100 group-hover:w-full"></span>
+        </li>
+
+        <li className="cursor-pointer relative group hover:after:w-full text-12 font-normal">
+          Itens
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-yellow-main opacity-0 transition-all duration-400 group-hover:opacity-100 group-hover:w-full"></span>
+        </li>
+        <li className="cursor-pointer relative group hover:after:w-full text-12 font-normal">
+          Localizações
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-yellow-main opacity-0 transition-all duration-400 group-hover:opacity-100 group-hover:w-full"></span>
+        </li>
       </ul>
     </div>
   );
